de295afb500f06fca3d54601c0f01c47495b3c43
USER-PC <USER-PC@ANGEL>
2024-02-22 23:40:36 -0500

    Add project files.

---
+++ Ogechi_Ikediashi_PROG8051_Gem_Hunters_A2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogechi_Ikediashi_PROG8051_Gem_Hunters_A2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Project Initiallization
+        }
+    }
+}
