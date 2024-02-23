11e6d0e1f46db2cec293a368c46f6fc736b8493c
USER-PC <USER-PC@ANGEL>
2024-02-23 17:14:51 -0500

    Creation of Player class

--- Ogechi_Ikediashi_PROG8051_Gem_Hunters_A2/Program.cs
+++ Ogechi_Ikediashi_PROG8051_Gem_Hunters_A2/Program.cs
@@ -18,14 +18,13 @@ namespace Ogechi_Ikediashi_PROG8051_Gem_Hunters_A2
 
 
 
-
     // This class represents a position on the game board with X and Y as Rows(Horizontal) and Columns(Vertical) respectively like in a table
     class Position
     {
-        // Public property declaration for X (RowPosition)
+        // Property declaration for X (RowPosition)
         public int X { get; set; }
 
-        // Public property declaration for Y (ColumnPosition)
+        // Property declaration for Y (ColumnPosition)
         public int Y { get; set; }
 
         // A constructor to initialize a Position, specifying X and Y
@@ -38,6 +37,50 @@ namespace Ogechi_Ikediashi_PROG8051_Gem_Hunters_A2
             Y = y;
         }
     }
+
+
+
+    // This class represents a player in the game
+    class Player
+    {
+        // Property for the player's name, with read-only
+        public string Name { get; }
+
+        // Property for the player's position on the game board
+        public Position Position { get; set; }
+
+        // Property for the count of gems collected by the player
+        public int GemCount { get; set; }
+
+        // Constructor to initialize a player with a name and starting position
+        public Player(string name, Position startPosition)
+        {
+            // Set the player's name using the provided value
+            Name = name;
+
+            // Set the player's position using the provided starting position
+            Position = startPosition;
+
+            // Initialize the count of gems collected by the player to 0
+            GemCount = 0;
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
