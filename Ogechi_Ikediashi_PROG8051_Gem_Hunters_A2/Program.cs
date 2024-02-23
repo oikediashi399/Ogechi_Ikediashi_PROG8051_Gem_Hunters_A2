7c5d6d27193db82ff9ac22d5c150c7b7138b1e2e
USER-PC <USER-PC@ANGEL>
2024-02-23 11:27:22 -0500

    Implemented the Position class with explicit comments

--- Ogechi_Ikediashi_PROG8051_Gem_Hunters_A2/Program.cs
+++ Ogechi_Ikediashi_PROG8051_Gem_Hunters_A2/Program.cs
@@ -8,9 +8,46 @@ namespace Ogechi_Ikediashi_PROG8051_Gem_Hunters_A2
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             // Project Initiallization
+            // Nothing here yet, still building the game
+
         }
     }
+
+
+
+
+    // This class represents a position on the game board with X and Y as Rows(Horizontal) and Columns(Vertical) respectively like in a table
+    class Position
+    {
+        // Public property declaration for X (RowPosition)
+        public int X { get; set; }
+
+        // Public property declaration for Y (ColumnPosition)
+        public int Y { get; set; }
+
+        // A constructor to initialize a Position, specifying X and Y
+        public Position(int x, int y)
+        {
+            // Setting the X to the provided parameter x (its passed value)
+            X = x;
+
+            // Setting the Y to the provided parameter y (its passed value)
+            Y = y;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
